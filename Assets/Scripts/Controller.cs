319c897eb175dbec037d56ff1186993367c46e60
Hethix <An.ryge@gmail.com>
2015-09-20 17:02:08 +0200

    Fixing things that got deleted!
    Trying to stun enemies and players when they get hit by projectiles

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -14,6 +14,8 @@ public class Controller : MonoBehaviour {
 	//Candy projectile prefab
 	public Rigidbody candyProjectile;
 
+	private float nextShot;
+	private float shotDelay = 0.5f;
 
 	// Use this for initialization
 	void Start () {
@@ -42,10 +44,14 @@ public class Controller : MonoBehaviour {
 
 
 		//Throwing Candy
-		if(Input.GetButtonDown(controllerNumber + "Fire1")) {
+		if(nextShot < Time.time && (Input.GetAxis(controllerNumber + "Fire1") < 0)) {
+			//if(candy <=0) not abble to throw candy
+			Instantiate(candyProjectile, transform.position, Quaternion.identity);
 
+			Rigidbody ins = Instantiate(candyProjectile, transform.position + (transform.forward*2), Quaternion.identity) as Rigidbody;
+			ins.AddForce(transform.forward * 35, ForceMode.VelocityChange);
+			nextShot = Time.time + shotDelay;
 		}
-
 
 		
 	}
