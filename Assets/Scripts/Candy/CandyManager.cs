8e65fbe32dbaada157f2f47764e4150c8a9b1091
Frans Peter Larsen <larsen.frans@gmail.com>
2015-09-20 21:06:24 +0200

    Skybox, lightning and shit

--- Assets/Scripts/Candy/CandyManager.cs
+++ Assets/Scripts/Candy/CandyManager.cs
@@ -5,7 +5,7 @@ public class CandyManager : MonoBehaviour {
 	int counter = 0;
 	int timeToNewSpawn = 100;
 	//load prefab of candy
-	public GameObject prefab;
+	public GameObject[] prefab;
 	
     // Instantiate the prefab somewhere between -10.0 and 10.0 on the x-z plane 
     void Start() {
@@ -15,7 +15,7 @@ public class CandyManager : MonoBehaviour {
 	void SpawnCandy(){
 		for(int i = 0; i<5; i++){
         	Vector3 position = new Vector3(Random.Range(-40.0F, 40.0F), 0.56F, Random.Range(-40.0F, 40.0F));
-        	Instantiate(prefab, position, Quaternion.identity);
+        	Instantiate(prefab[(int) Random.Range(0, prefab.Length)], position, Quaternion.identity);
     	}	
 	}
 	
