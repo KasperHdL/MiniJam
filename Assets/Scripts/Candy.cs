2b2725437934270fbb64242610f9cbcc03b1dd50
Maja Daniela Hansen <MajaDanielaHansen@Gmail.com>
2015-09-20 11:44:56 +0200

    Random spawning candy pieces and player collision with candy.

---
+++ Assets/Scripts/Candy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Candy : MonoBehaviour {
+
+	//load sound of picking up candy
+	//public AudioClip pickUpCandySound;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision){
+		Debug.Log("Collision");
+		//play sound
+		//AudioSource.PlayClipAtPoint(pickUpCandySound, transform.position);
+		//destroy
+		gameObject.SetActive (false);
+	}
+}
+
