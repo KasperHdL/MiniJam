8c9fb619630b1e42867f3bf931dfd46e9743eaf5
Maja Daniela Hansen <MajaDanielaHansen@Gmail.com>
2015-09-20 17:03:59 +0200

    Each Player have own bag to increase.

---
+++ Assets/Scripts/Candy/CandyBagIncrease.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CandyBagIncrease : MonoBehaviour {
+	//gameObject CandyBag;
+	public CandyBag cb;
+
+	// Use this for initialization
+	void Start () {
+		cb = GameObject.Find ("CandyBag").GetComponent<CandyBag> ();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localScale = new Vector3(0.5F+cb.weightPlayer1,1+cb.weightPlayer1,0.5F+cb.weightPlayer1);
+		transform.localScale = new Vector3(0.5F+cb.weightPlayer2,1+cb.weightPlayer2,0.5F+cb.weightPlayer2);
+		transform.localScale = new Vector3(0.5F+cb.weightPlayer3,1+cb.weightPlayer3,0.5F+cb.weightPlayer3);
+		transform.localScale = new Vector3(0.5F+cb.weightPlayer4,1+cb.weightPlayer4,0.5F+cb.weightPlayer4);
+	}
+}
