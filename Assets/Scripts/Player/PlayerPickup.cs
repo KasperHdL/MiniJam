243e30d0a7e3d6b6362f922af363e658be8dfa4b
KasperHdL <kasper@honnens.dk>
2015-09-20 18:29:23 +0200

    player pickup ..

---
+++ Assets/Scripts/Player/PlayerPickup.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerPickup : MonoBehaviour {
+
+
+	Type currentPickup;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(currentPickup != Type.None && Input.GetButtonDown(controllerName + "Fire2")){
+
+		}
+	}
+
+
+	public void SetPickup(PlayerPickup.Type pickup){
+		currentPickup = pickup;
+	}
+
+	public enum Type{
+		None,
+		Stun
+	}
+
+}
+
