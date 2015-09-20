153deb2063860511978d7adb1076cac084be5982
KasperHdL <kasper@honnens.dk>
2015-09-20 11:46:25 +0200

    Monster Spawner and Simple Chase AI for monster implemented

---
+++ Assets/PlayerHandler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHandler : MonoBehaviour {
+
+
+	public static Transform[] PLAYERS;
+
+	// Use this for initialization
+	void Awake () {
+		PLAYERS = new Transform[transform.childCount];
+
+		int i = 0; 	
+
+		foreach(Transform child in transform){
+			PLAYERS[i++] = child;
+		}
+	}
+}
